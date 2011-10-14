c719963091296b9ee621de805768f09b2bfcfbde
Carlos Rodriguez <carlos@eddorre.com>
2011-10-13 17:03:19 -0700

    Using feature detection to fix deprecation warnings in Rails ~> 2.3.9

--- rails/init.rb
+++ rails/init.rb
@@ -3,8 +3,17 @@ RAILS_DEFAULT_LOGGER.info "** vote_fu: setting up load paths"
 %w{ models controllers helpers }.each do |dir|
   path = File.join(File.dirname(__FILE__) , 'lib', dir)
   $LOAD_PATH << path
-  ActiveSupport::Dependencies.load_paths << path
-  ActiveSupport::Dependencies.load_once_paths.delete(path)
+  if ActiveSupport::Dependencies.respond_to?(:autoload_paths)
+    ActiveSupport::Dependencies.autoload_paths << path
+  else
+    ActiveSupport::Dependencies.load_paths << path
+  end
+
+  if ActiveSupport::Dependencies.respond_to?(:autoload_once_paths)
+    ActiveSupport::Dependencies.load_once_paths.delete(path)
+  else
+    ActiveSupport::Dependencies.autoload_once_paths.delete(path)
+  end
 end
 
 require 'vote_fu'
